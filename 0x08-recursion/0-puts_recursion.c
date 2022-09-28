bf544d30f77ff5a3e7ebaa721154edbac7e59196
KSam001 <samcapital.inc@gmail.com>
2022-09-28 14:29:52 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,23 +1,16 @@
 #include "main.h"
-
 /**
-* _puts_recursion - Prints a string followed by a new line
-* @s: string
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-
-
-
-void _puts_recursion(char *s)
+ * _print_rev_recursion - print a string in reverse
+ * @s: the string
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
 {
-    if (*s == 0)
-    {
-        _putchar('\n');
+    if (!*s)
         return;
+    if (*s)
+    {
+        _print_rev_recursion(s + 1);
+        _putchar(*s);
     }
-    _putchar(*s);
-    _puts_recursion(s + 1);
 }
-
